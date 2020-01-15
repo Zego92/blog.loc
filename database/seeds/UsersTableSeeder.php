8747ac44b993c0d6725311624d2782395facce66
Zego92 <Timchenco92@gmail.com>
2020-01-15 23:06:38 +0200

    Add first code adn add project files

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'role_id' => '1',
+            'name' => 'MD.Admin',
+            'username' => 'admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        DB::table('users')->insert([
+            'role_id' => '2',
+            'name' => 'MD.Author',
+            'username' => 'author',
+            'email' => 'author@test.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
